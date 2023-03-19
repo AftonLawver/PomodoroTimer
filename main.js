8201f627db69008a81f1d284e1417c247e48f509
afton21 <lawverap25@gmail.com>
2023-03-19 08:27:22 -0500

    Made all main functionalities of the user interface work (pause timer, start timer, stop timer).

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ app.whenReady().then(() => {
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
-            createMainWindow();
+            createMainWindow(); 
         }
     })
 });
--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -10,7 +10,6 @@ function displayClock() {
     setTimeout(displayClock, 1000)
 }
 
-
 if (time.charAt(time.length-2) === 'P') {
     let hour = new Date().getHours();
     if (hour >= 16) {
@@ -27,20 +26,13 @@ else {
 let button25Minutes = document.getElementById("25_minute_button");
 let button50Minutes = document.getElementById("50_minute_button");
 button25Minutes.onclick = function() {
-    document.getElementById("label1").remove();
-    document.getElementById("25_minute_button").remove();
-    document.getElementById("50_minute_button").remove();
-    document.getElementById("progressBar").style.visibility = "visible";
+    switchButtons()
     progress(10, 10, $('#progressBar'));
 }
 
 button50Minutes.onclick = function() {
-    document.getElementById("label1").remove();
-    document.getElementById("25_minute_button").remove();
-    document.getElementById("50_minute_button").remove();
-    document.getElementById("progressBar").style.visibility = "visible";
+    switchButtons();
     progress(3000, 3000, $('#progressBar'));
-
 }
 
 function progress(timeleft, timetotal, $element) {
@@ -53,18 +45,70 @@ function progress(timeleft, timetotal, $element) {
     $element.find('div').animate({ width: progressBarWidth }, 300).html(Math.floor(timeleft/60) + ":" + seconds);
 
     if(timeleft > 0) {
-        setTimeout(function() {
+        document.getElementById('stop_button').onclick = function() {
+            setBackToMain();
+            clearTimeout(timeoutId);
+            return;
+        };
+        document.getElementById('pause_button').onclick = function() {
+            document.getElementById("pause_button").style.display = 'none';
+            document.getElementById("start_button").style.display = 'inline';
+            clearTimeout(timeoutId);
+            document.getElementById('start_button').onclick = ()=> {
+                document.getElementById("start_button").style.display = 'none';
+                document.getElementById("pause_button").style.display = 'inline';
+                progress(timeleft, timetotal, $element);
+            }
+        };
+        const timeoutId = setTimeout(()=> {
             progress(timeleft - 1, timetotal, $element);
         }, 1000);
     }
     else {
         document.getElementById('alarm').play();
+
     }
 };
 
-function replaceProgressBar() {
-    const timer = document.getElementById('progressBar');
-    const breakQuestion = document.createElement('div');
+function switchButtons() {
+    document.getElementById("spot1").style.display = 'none';
+    document.getElementById("25_minute_button").style.display = 'none';
+    document.getElementById("50_minute_button").style.display = 'none';
+    document.getElementById("stop_button").style.display = 'inline';
+    document.getElementById("pause_button").style.display = 'inline';
+    document.getElementById("progressBar").style.display = 'block';
 }
+
+function setBackToMain() {
+    document.getElementById("spot1").style.display = 'block';
+    document.getElementById("25_minute_button").style.display = 'inline';
+    document.getElementById("50_minute_button").style.display = 'inline';
+    document.getElementById("stop_button").style.display = 'none';
+    document.getElementById("pause_button").style.display = 'none';
+    document.getElementById("progressBar").style.display = 'none';
+    document.getElementById("start_button").style.display = 'none';
+
+}
+
+class Timeout {
+    constructor(callbackFunction, time) {
+        this.time = time;
+        this.callback = callbackFunction;
+        this.run(); // It will be automatically invoked when the constructor is run
+    }
+    run() {
+        this.startedTime = new Date().getTime();
+        if (this.time > 0) {
+            this.timeout = setTimeout(this.callback, this.time); // Timeout must be set if this.time is greater than 0
+        }
+    }
+    pause() {
+        let currentTime = new Date().getTime();
+        this.time = this.time - (currentTime - this.startedTime); // The time that was given when initializing the timeout is subtracted from the amount of time spent
+        clearTimeout(this.timeout);
+    }
+}
+
+
 
 
