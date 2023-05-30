ade4ef2722399105e1a630639167e253dbe59cc9
Afton Lawver <lawverap25@gmail.com>
2023-05-30 11:08:41 -0500

    Modified main.js to work with new title bar. Need to update renderer process (renderer.js)

--- main.js
+++ main.js
@@ -4,37 +4,67 @@
 const path = require('path');
 const { app, BrowserWindow, ipcMain } = require('electron');
 
-process.env.NODE_ENV = 'production';
+// process.env.NODE_ENV = 'production';
 
-// const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== 'production';
 const isMac = process.platform === 'darwin';
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Pomodoro Timer',
-        // width: isDev ? 600 : 400,
-        width: 450,
+        width: isDev ? 600 : 400,
+        // width: 450,
         height: 350,
-        minHeight: 350,
-        maxHeight: 350,
-        minWidth: 450,
-        maxWidth: 450,
-        autoHideMenuBar: true,
+        // minHeight: 350,
+        // maxHeight: 350,
+        // minWidth: 450,
+        // maxWidth: 450,
+        frame: false,
         preload: path.join(__dirname, 'preload.js'),
         webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
+            nodeIntegration: true,
             sandbox: false,
+
+            //This is the statement that gives me problems.
+            // By changing contextIsolation: false it renders the jquery inaccessible
+            contextIsolation: true,
+
+
+
         },
     });
-    // ipcMain.handle('ping', () => 'pong');
-
-    // Open devtools if in dev env
-    // if (isDev) {
-    //     mainWindow.webContents.openDevTools();
-    // }
-
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
     mainWindow.loadFile(path.join(__dirname, './renderer/mainWindow.html'));
+
+    // CLOSE APP
+    ipc.on('closeApp', ()=> {
+        mainWindow.close();
+    })
+
+    // MINIMIZE APP
+    ipc.on('minimizeApp', ()=> {
+        mainWindow.minimize();
+    })
+
+    mainWindow.on('maximize', ()=>{
+        mainWindow.webContents.send('isMaximized');
+    })
+
+    mainWindow.on('unmaximize', ()=>{
+        mainWindow.webContents.send('isRestored')
+    })
+
+    // MAXIMIZE APP
+    ipc.on('maximizeRestoreApp', ()=> {
+        if (mainWindow.isMaximized()) {
+            mainWindow.restore();
+        }
+        else {
+            mainWindow.maximize();
+        }
+    })
 }
 
 // App is ready
@@ -44,7 +74,7 @@ app.whenReady().then(() => {
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
-            createMainWindow(); 
+            createMainWindow();
         }
     })
 });
